d2504e3fdaae3a671a4ff1c56243d346bb02fa27
Daniel Malcher <dbm.eng18@uea.edu.br>
2023-06-27 10:49:41 -0400

    Removing unused assets

--- Assets/Script/Packaging/PackagingController.cs
+++ Assets/Script/Packaging/PackagingController.cs
@@ -7,7 +7,7 @@ public class PackagingController : MonoBehaviour
     [SerializeField]private GameObject clawPackaging;
 
     private Animator packagingAnimator;
-    // Start is called before the first frame update
+
     void Start()
     {
         packagingAnimator = clawPackaging.GetComponent<Animator>();
