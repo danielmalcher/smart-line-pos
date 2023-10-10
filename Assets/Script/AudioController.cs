f0ba9f9c13c333673a9642df5df518c4b088df76
danielmalcher <dbm.eng18@uea.edu.br>
2023-10-10 10:56:51 -0400

    Fix Audio pause/resume issues

--- Assets/Script/AudioController.cs
+++ Assets/Script/AudioController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     [SerializeField]private List<AudioSource> moduleExplanations;
+    private AudioSource pausedAudio;
 
     void StopOverlappingAudio()
     {
@@ -17,7 +18,37 @@ public class AudioController : MonoBehaviour
 
     public void PlayAudio(int moduleNumber)
     {
+        if(pausedAudio){
+            EmptyPausedAudio();
+        }
         StopOverlappingAudio();
         moduleExplanations[moduleNumber].Play();
+    }
+
+    void PauseAudio(){
+        for(int i = 0; i <= moduleExplanations.Count -1; i++){
+            if(moduleExplanations[i].isPlaying){
+                moduleExplanations[i].Pause();
+                pausedAudio = moduleExplanations[i];
+            }
+        }
+    }
+
+    void ResumeAudio(){
+        pausedAudio.UnPause();
+        pausedAudio = null;
+    }
+
+    void EmptyPausedAudio(){
+        pausedAudio.Stop();
+        pausedAudio = null;
+    }
+
+    public void ManageAudio(){
+        if(pausedAudio){
+            ResumeAudio();
+        } else {
+            PauseAudio();
+        }
     }
 }
